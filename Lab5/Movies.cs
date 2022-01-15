8f3e2f2e6db23fdbf661027b5bacea0d728e6ebb
Sqzjelele <jakub.kowalczyk1280@gmail.com>
2022-01-15 20:06:58 +0100

    Add project files.

---
+++ Lab5/Movies.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Lab5
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string ReleaseDate { get; set; }
+
+        public int NumberOfReviews { get; set; }
+
+        public int RuntimeInMinutes { get; set; }
+
+        public string Title { get; set; }
+    }
+}
